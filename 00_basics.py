62055f5f8450f361584b1c54e0655ebc453bc3f8
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2026-03-03 15:33:12 +0000

    Excersises complete

--- 00_basics.py
+++ 00_basics.py
@@ -1,20 +1,24 @@
 # Exercise One
 # Print the string "hello world"
-
+print("Hello World!")
 # Exercise Two
 # Create a variable called "name" and assign it your name as a string. Then print the variable.
-
+name = "Jacob"
+print(name)
 # Exercise Three
 # Create a variable called "age" and assign it your age as an integer. Then print the variable.
-
+age = 18
+print(age)
 # Exercise Four
 # Create a variable called "is_student" and assign it a boolean value indicating whether you are a student or not. Then print the variable. 
-
+is_student = True
+print(is_student)
 # Exercise Five
 # Print the string "Hello, <name>", where <name> is the variable defined in Exercise Two.
-
+print("Hello " + name)
 # Exercise Six
 # Print the string "Hello, <name>, your age is <age>.".
-
+print("Hello " + name, " your age is " + str(age))
 # Exercise Seven
 # Print the string "Hello, <name>, your age is <age> and your student status is <is_student>."
+print("Hello " +name, "Your age is " +str(age), "and your student status is " + str(is_student))
--- 01_variables_and_functions.py
+++ 01_variables_and_functions.py
@@ -1,21 +1,33 @@
 # Exercise One
 # Create a function called `greet_world` that prints "Hello World"
+def greet_world():
+    print("Hello World")
 # Execute the `greet_world` function to see the output.
-
+greet_world()
 # Exercise Two
 # Create a function called `greet_person` that takes a name as an argument and prints "Hello, [name]!"
+def greet_person(name):
+    print("Hello " +name)
 # Execute the `greet_person` function with your name to see the output.
-
+greet_person("Jacob")
 # Exercise Three
 # Create a function called `greet_person_with_age` that takes a name and an age as arguments and prints "Hello, [name]! You are [age] years old."
+def greet_person_with_age(name,age):
+    print("Hello " +name, "You are " +str(age), "years old.")
 # Execute it with your name and age to see the output.
-
+greet_person_with_age("Jacob",18)
 # Exercise Four
 # Create a variable x and assign it the value 10. 
+x = 10
 # Create a variable y and assign it the value 5.
+y = 5
 # Create a variable sum and assign it the result of adding x and y together.
+sum = x + y
 # Print the value of sum to see the result.
-
+print(sum)
 # Exercise Three
 # Create a function called `add_numbers` that takes two numbers as arguments and returns their sum.
+def add_numbers():
+    print(5 + 17)
 # Call the `add_numbers` function with two numbers and print the result to see the output.
+add_numbers()
