ff5cf0f7ec69a60c05432561281e684a7a0529d6
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-03-03 15:31:03 +0000

    add examples to name, age, boolean exercises

--- 00_basics.py
+++ 00_basics.py
@@ -8,13 +8,18 @@
 # Create a variable called "age" and assign it your age as an integer. Then print the variable.
 
 # Exercise Four
-# Create a variable called "is_student" and assign it a boolean value indicating whether you are a student or not. Then print the variable. 
+# Create a variable called "is_student" and assign it a boolean value indicating whether you are a student or not. Then print the variable.
 
 # Exercise Five
 # Print the string "Hello, <name>", where <name> is the variable defined in Exercise Two.
+# e.g. if name is "Alice", your output should be: Hello, Alice
 
 # Exercise Six
 # Print the string "Hello, <name>, your age is <age>.".
+# e.g. if name is "Alice" and age is 20, your output should be: Hello, Alice, your age is 20.
+
 
 # Exercise Seven
 # Print the string "Hello, <name>, your age is <age> and your student status is <is_student>."
+# e.g. if name is "Alice", age is 20 and the person is a student, it should print:
+# Hello Alice, your age is 20 and your student status is True
