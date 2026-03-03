85707218651a774216ce2e8aa9f314f587e2fd3c
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2026-03-03 16:23:36 +0000

    more excersises completed

--- 02_conditionals_and_loops.py
+++ 02_conditionals_and_loops.py
@@ -1,23 +1,47 @@
 # Exercise One
 # Create a boolean variable and assign it a value of either True or False
+boolean_variable = True
 # Create an if / else statement that prints "is true" when the variable is True, and "is false" when the variable is False.
+if boolean_variable:
+    print("True!")
+else:
+    print("False!")
 # Run the script and change the variable's value to test the difference.
 
 # Exercise Two
 # Create a variable with name x and assign it the value 10
+x = 10
 # Create a variable with name y and assign it the value 5
+y = 5
 # Create an if / else statement that prints "x is higher than y" or "y is higher than x" accordingly.
+if x > y:
+    print("X is higher than Y")
+else:
+    print("Y is higher than X")
 # Run the script and switch the values of x and y around to test the difference.
 
 
 # Exercise Three
 # Create a function called `is_even` that takes a number as an argument and returns True if the number is even, and False if it is odd.
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 # Hint: You may need to use google to learn how to test if a number is odd or even!
 # Call the `is_even` function with an even number and print the result to see the output.
+test = is_even(22)
+print(test)
 # Call the `is_even` function with an odd number and print the result to see the output.
-
+print(is_even(15))
 # Exercise Four
 # Create a while loop that loops through 100 values of a variable `i` and prints them
-
+i = 0
+while i in range(100):
+    print(i)
+    i += 1
 # Execise Five
 # Create a for loop that achieves the same as the loop in exercise Four
+i = 0
+for i in range(100):
+    print(i)
--- 03_arrays.py
+++ 03_arrays.py
@@ -1,31 +1,49 @@
 # Exercise One
 # Create an array called `fruits` that contains the following strings: "apple", "banana", "cherry".
+array = ["apple", "banana", "cherry"]
+print(array)
 
 # Exercise Two
 # Print the length of the `fruits` array.
+print(len(array))
 
 # Exercise Three
 # Add the string "orange" at the end of the `fruits` array.
+array.append("orange")
 # Print the `fruits` array to see the changes.
+print(array)
 
 # Exercise Four
 # Remove the last element from the `fruits` array.
+array.pop()
 # Print the `fruits` array to see the changes.
+print(array)
 
 # Exercise Five
 # Remove the first element from the `fruits` array.
+array.pop(0)
 # Print the `fruits` array to see the changes.
+print(array)
 
 # Exercise Six
 # Modify the code form Exercise Five to remove the second element from the `fruits` array instead.
+new_array = ["Mercury", "Venus", "Earth", "Mars"]
+print(new_array)
+new_array.pop(1)
+print(new_array)
 
 # Exercise Seven
 # Create a new array called `more_fruits` that contains the following strings: "grape", "melon".
+more_fruits = ["grape", "melon"]
 # Combine the `fruits` and `more_fruits` arrays into a new array called `all_fruits`.
+all_fruits = array + more_fruits
 # Print the `all_fruits` array to see the combined list of fruits.
+print(all_fruits)
 
 # Exercise Eight
 # Create a function called `add_fruit` that takes an array of fruits as an argument and a string representing a new fruit.
+def add_fruit(fruits):
+    
 # The function should add the new fruit to the array and return the updated array.
 # Call the `add_fruit` function with the `all_fruits` array and a new fruit, then print the result.
 
